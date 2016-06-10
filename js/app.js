1c1d7fdccce6d3176257b69286d29e8a1646bb3e
Conner Dart <conner.j.dart@gmail.com>
2016-06-09 19:34:37 -0500

    Updates to css and json

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('.media').change(function() {
+	$('select.input.media').change(function() {
 		var media_type = $(this).val();
 		if (media_type == "Article" || media_type == "Video") {
 			$('#link').attr('placeholder', "Link").val('').focus().blur();
@@ -9,7 +9,7 @@ $(document).ready(function() {
 		}
 	})
 
-	$('.elaborate').change(function() {
+	$('select.input.elaborate').change(function() {
 		var elaboration = $(this).val();
 		if (elaboration == "Note") {
 			$('#note').attr('placeholder', "Note").val('').focus().blur();
@@ -19,11 +19,26 @@ $(document).ready(function() {
 		}
 	})
 
-	$('.list-elaborate').change(function() {
-		var yes = $(this).val();
-		if (yes = "Yes") {
-			$('#note').replaceWith('<ul><li></li><li></li><li></li></ul>')
-		}
+	$('input.another-one.list').click(function() {
+		$('ul.ul-notes').append('<li><label><textarea class="input note" rows="1" id="note" placeholder="Whatcha got to say?"></textarea></label></li>');
+		console.log("it works");
+	})
+
+	$('input.archive').click(function() {
+		var media_type = $('select.input.media').val();
+		console.log(media_type)
+		var media = $('textarea#link.input.link').val();
+		console.log(media)
+		var elaboration_type = $('select.input.elaborate').val();
+		console.log(elaboration_type)
+		var elaboration = []
+		$('ul.ul-notes').find('textarea#note.input.note').each(function() {
+			note = $(this).val();
+			elaboration.push(note);
+		})
+		console.log(elaboration)
+		var how_often = $('select.input.howoften').val();
+		console.log(how_often)
 	})
 	
 })
